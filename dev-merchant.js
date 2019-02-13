fe90c4f9a697dbc40a488d4a50173a143b2aae2d
shadurin <igor.shadurin@gmail.com>
2019-02-13 13:45:22 +0300

    init

---
+++ dev-merchant.js
@@ -0,0 +1,7 @@
+console.log('ok');
+window.onload = function () {
+    let iframe = document.createElement('iframe');
+    //iframe.style.display = "none";
+    iframe.src = 'http://localhost:3001/';
+    document.body.appendChild(iframe);
+};
