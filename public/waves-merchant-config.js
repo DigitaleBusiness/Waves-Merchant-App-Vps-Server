c6354e2cfc9e30c443e27cf8595443c26a82546d
DigitaleBusiness <56487752+DigitaleBusiness@users.noreply.github.com>
2019-10-26 22:44:21 +0200

    Update waves-merchant-config.js

--- public/waves-merchant-config.js
+++ public/waves-merchant-config.js
@@ -1,16 +1,16 @@
 const WAVES_MERCHANT_CONFIG = {
     notification_url: './mail/waves-mail.php',
     item_title: 'WAVES Merchant',
-    allowed_tokens: ['WAVES', 'TRY', 'LTC', 'ETH', 'BTC'],
-    recipient_wallet: '3P3mWFmANJ8xkM1UY3CWHYqsqfm62v2g23x',
+    allowed_tokens: ['WAVES', 'BTC', 'ETH', 'BDX'],
+    recipient_wallet: '3PGZNhPeThgyGsxWHZPjRxLiDYjYjzopBqF',
     text_buy_button: 'Buy for {token}',
     waves_data_service: 'https://api.wavesplatform.com/',
     /*exchange_rates: {
         'USD': {
             'WAVES': '2.88',
-            'ETH': '140',
-            'WCT': '0.8',
             'BTC': '4000',
+            'ETH': '140',
+            'BDX': '0,09',
         }
     }*/
 };
